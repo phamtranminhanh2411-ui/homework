6f524070b3e24cbc0cb9e138203a0c1674f9fc6b
Minh Anh <phamtranminhanh2411@gmail.com>
2026-07-22 00:18:33 +0700

    Add cart total calculation

--- homework1.js
+++ homework1.js
@@ -1,2 +1,26 @@
 "use strict";
 console.log("Homework 1: Shopping Cart");
+
+const cart = [ {
+    name: "Sunflower",
+    price: 29000,
+    quantity: 10
+},
+
+{
+    name: "Rose",
+    price: 25000,
+    quantity: 5
+},
+]
+
+function calculateTotal(cart) {
+    let total = 0;
+    for (let i = 0; i < cart.length; i++) {
+
+        total += cart[i].price * cart[i].quantity;}
+    
+    return total;
+}
+const total = calculateTotal(cart);
+console.log("Total price: " + total);
